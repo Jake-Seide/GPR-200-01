a5d4a8d65566914e879d12634e9c45c36d91ef68
Jake Seide <jake.seide@mymail.champlain.edu>
2020-12-09 18:53:56 -0500

    Adding Bloom passes to final
    * Compatable with existing code
    * Output should be a vertex displacement shader with a bloom shader -> work best with sphere

---
+++ Final_Main/BlurPass/BlurPass_VS.glsl
@@ -0,0 +1,22 @@
+#version 330
+
+//MAIN DUTY: READ ATTRIBUTES
+//e.g. 3D pos
+//e.g. surface normal
+//e.g. uv: texture coord
+
+//Attributes
+layout (location = 0) in vec4 aPosition;
+layout (location = 2) in vec2 aTexcoord;
+
+//Varying(vs)
+out vec4 vPosition;
+out vec2 vTexcoord;
+
+void main()
+{
+	gl_Position = aPosition;
+
+	vTexcoord = aTexcoord;
+	vTexcoord = aPosition.xy * 0.5 + 0.5;
+}
