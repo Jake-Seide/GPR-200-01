d799616745725ac7db8815808fcb92156c74e701
Jake Seide <jake.seide@mymail.champlain.edu>
2020-12-09 21:17:38 -0500

    Adding mouse interaction and comments

--- Final_Main/TonePass/TonePass_PS.glsl
+++ Final_Main/TonePass/TonePass_PS.glsl
@@ -1,11 +1,16 @@
 #version 330
 
+//Output variable
 layout (location = 0) out vec4 rtFragColor;
 
+//Varying(s) 
 in vec2 vTexcoord;
 
+//Uniforms
 uniform sampler2D uTex;
 
+// Getting Luminacity color for each pixel
+// Referening CookBook By: David Wolff
 float Luma (in vec3 color)
 {
     return 0.2126 * color.r + 
@@ -13,16 +18,21 @@ float Luma (in vec3 color)
            0.0722 * color.b;
 }
 
+// Brightening and darkening pixels based on luminocity
+// Referening CookBook By: David Wolff
 vec4 TonePass(in vec2 fragCoord)
 {
+	//Getting ScenePass
 	vec2 uv = vTexcoord.xy;
 	vec4 val = texture(uTex, vTexcoord.xy);
 	
+	//Getting Constriants for Tone Effect
 	return val * clamp(Luma(val.rgb) - 0.5, 0.0, 1.0) *
 			(1.0 / (1.0 - 0.5));
 }
 
 void main()
 {
+	//Outputs Tone Effect
 	rtFragColor = TonePass(vTexcoord); //texture(uTex, vTexcoord.xy);
 }
--- Final_Main/TonePass/TonePass_VS.glsl
+++ Final_Main/TonePass/TonePass_VS.glsl
@@ -1,24 +1,21 @@
 #version 330
 
+//Atributes
 layout (location = 0) in vec4 aPosition;
 layout (location = 2) in vec2 aTexcoord;
 
 //Uniforms
 uniform mat4 uModelMat, uViewMat, uProjMat, uViewProjMat;
 
+//Varyings
 out vec2 vTexcoord;
 
 void main()
 {
+	//Setting Position Attributes
 	gl_Position = aPosition;
-
-	//mat4 modelViewProjMat = uProjMat * uViewMat * uModelMat;
-	//gl_Position = modelViewProjMat * aPosition;
+	
+	//Set Texture Varyings
 	vTexcoord = aTexcoord;
 	vTexcoord = aPosition.xy * 0.5 + 0.5;
-
-	//mat4 modelViewProjMat = uProjMat * uViewMat * uModelMat;
-	//gl_Position = modelViewProjMat * aPosition;
-	//vTexcoord = aTexcoord;
-	//vTexcoord = TonePass(vTexcoord.xy);
 }
